b0c9221704f2116372b8ed96ca8acff1fe403384
Ellery Kreloff <ellery.kreloff@gmail.com>
2021-03-29 23:43:56 -0400

    version 1 of link

--- src/components/Board/components/DefaultCard/index.js
+++ src/components/Board/components/DefaultCard/index.js
@@ -1,17 +1,37 @@
-export default function ({ children: card, dragging, allowRemoveCard, onCardRemove }) {
-  return (
-    <div className={`react-kanban-card ${dragging ? 'react-kanban-card--dragging' : ''}`}>
-      <span>
-        <div className='react-kanban-card__title'>
-          <span>{card.title}</span>
-          {allowRemoveCard && (
-            <span style={{ cursor: 'pointer' }} onClick={() => onCardRemove(card)}>
-              ×
-            </span>
-          )}
-        </div>
-      </span>
-      <div className='react-kanban-card__description'>{card.description}</div>
-    </div>
-  )
+export default function ({ children: card, dragging, allowRemoveCard, onCardRemove, commissionLink=null }) {
+  if (commissionLink !== null){
+    return (
+      <div className={`react-kanban-card ${dragging ? 'react-kanban-card--dragging' : ''}`}>
+        <span>
+          <div className='react-kanban-card__title'>
+            <span>{card.title}</span>
+            {allowRemoveCard && (
+              <span style={{ cursor: 'pointer' }} onClick={() => onCardRemove(card)}>
+                ×
+              </span>
+            )}
+          </div>
+        </span>
+        <div className='react-kanban-card__description'>{card.description}</div>
+        <a href={commissionLink}>View</a>
+      </div>
+    )
+  }
+  else{
+    return (
+      <div className={`react-kanban-card ${dragging ? 'react-kanban-card--dragging' : ''}`}>
+        <span>
+          <div className='react-kanban-card__title'>
+            <span>{card.title}</span>
+            {allowRemoveCard && (
+              <span style={{ cursor: 'pointer' }} onClick={() => onCardRemove(card)}>
+                ×
+              </span>
+            )}
+          </div>
+        </span>
+        <div className='react-kanban-card__description'>{card.description}</div>
+      </div>
+    )
+  }
 }
